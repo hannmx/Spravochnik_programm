eb570dabd5570a0ebc641a0cdde56807e717fb6b
hannmx <mxkud@mail.ru>
2023-03-02 16:22:32 +0300

    Созданы начальные файлы для разработки программы "Телефонный справочник", добавлены файлы gitignore README

---
+++ main.py
@@ -0,0 +1,9 @@
+import functions_book as fb
+import functions_read_write_file as frw
+import checking_file as cf
+
+#Создание объекта Member и его методов#
+
+def __init__(self, listfields, arg):
+    
+
