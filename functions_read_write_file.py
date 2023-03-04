1bc46fbf1a9442808702da727ab3b2b594888f3b
hannmx <mxkud@mail.ru>
2023-03-04 13:12:52 +0300

    Реализован класс Member и его внутренние функции, и создана часть записи новых данных и чтение файла в отдельном программном файле

---
+++ functions_read_write_file.py
@@ -0,0 +1,13 @@
+import pickle # модуль для сохранения объектов в файл
+
+def readfile(name):
+    try:
+        with open(name, 'rb') as f:
+            return  pickle.load(f)
+    except:
+        print('\t\tДанных еще не сохранено!\n ')
+        return []
+        
+def savedata(obj,name):
+    file = open(name,'wb')
+    pickle.dump(obj , file)
--- main.py
+++ main.py
@@ -3,7 +3,34 @@ import functions_read_write_file as frw
 import checking_file as cf
 
 #Создание объекта Member и его методов#
+class Member:
 
-def __init__(self, listfields, arg):
+    #Словарь пользователя где ключи - имя поля а в значениях - значение этого поля
+    def __init__(self, listfields, arg):
+        self.data = dict(zip(listfields, arg))
+
+    #Возвращение имени и фамилии одной строкой
+    def identifier(self):
+        return ' '.join([self.data['Имя'], self.data['Фамилия']]).lower()
     
+    #Принимает ключ и новое значение. Изменяет значение по ключу
+            #или создает новую запись если ключа еще нет
+    def parametr(self, key, values):
+        self.data[key] = values
+
+    #Возвращает значение поля возраст
+    def years(self):
+        return self.data['возраст']
+ 
+    #Возвращает множество значений словаря в нижнем регистре
+    def show(self):
+        return set(map(str.lower, list(self.data.values())))
+ 
+    #Возвращает список ключей
+    def change(self):
+        return list(self.data.keys())
+ 
+    #Возвращает строку значений
+    def __str__(self):
+        return ' '.join(list(self.data.values()))
 
